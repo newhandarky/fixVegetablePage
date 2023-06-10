78553a1b7486a3322b36408b6cbae6eabf8430a0
newhandarky <84302055+newhandarky@users.noreply.github.com>
2023-06-11 01:00:30 +0800

    修改變數名稱 刪除不必要的下拉選單連帶切換 移除等待500毫秒的資料載入時間 新增網頁載入完畢顯示全部資料

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const search = document.querySelector(".search");
 const textCenter = document.querySelector(".text-center");
 const jsSelect = document.querySelector("#js-select");
 const jsMoblieSelect = document.querySelector("#js-moblie-select");
-const upAndDown = document.querySelector(".js-sort-advanced");
+const upDownSort = document.querySelector(".js-sort-advanced");
 
 const selectArr = ["上價", "中價", "下價", "平均價", "交易量"];
 let data = [];
@@ -14,11 +14,11 @@ let resultData = [];
 
 axios.get('https://hexschool.github.io/js-filter-data/data.json')
     .then(function (response) {
-        data = response.data;
+        // 取得資料時先移除null欄位項目
+        data = response.data.filter(item => item["作物名稱"] !== null);
     }).catch(function (error) {
         console.log(error);
     });
-
 
 search.addEventListener("click", function (e) {
     let getType = document.querySelector(".selected");
@@ -26,34 +26,25 @@ search.addEventListener("click", function (e) {
     // 重置resultData內容
     resultData = [];
 
-    if (getType === null) {
-        alert("請選擇要查詢的農產品類別");
+    if (getType === null || roundedEnd.value.trim() === "") {
+        roundedEnd.value.trim() === "" ? alert("請輸入要比價的農產品") : alert("請選擇要查詢的農產品類別");
         return;
     }
-    if (roundedEnd.value.trim() === "") {
-        alert("請輸入要比價的農產品");
-        return;
-    } else {
-        showResult.innerHTML = `查看「${roundedEnd.value}」的比價結果`;
-    }
+    showResult.innerHTML = `查看「${roundedEnd.value}」的比價結果`;
 
     data.forEach(function (item) {
-        // 排除資料內有null的物件避免出錯
-        if (item["作物名稱"] != null) {
-            if (item["種類代碼"] === getType.getAttribute("data-type"))
-                newData.push(item);
+        if (item["種類代碼"] === getType.getAttribute("data-type")) {
+            newData.push(item);
         }
     })
+
     newData.forEach(function (item) {
+        // 比對作物名稱, 有找到就丟到resultData內
         if (item["作物名稱"].indexOf(roundedEnd.value) !== -1) {
             resultData.push(item);
         }
     })
-    if (resultData.length == 0) {
-        renderList(resultData);
-    } else {
-        renderList(resultData);
-    }
+    renderList(resultData);
 })
 
 // 切換農產品類別選取狀態
@@ -65,43 +56,33 @@ let checkSelected = function (event) {
         event.target.classList.add("selected");
     }
 }
+// 幫選取的農產品按鈕種類上色
 btnGroups.addEventListener("click", function (e) {
-    if (e.target.nodeName !== "BUTTON") {
-        return;
-    } else if (e.target.getAttribute("data-type") === "N04") {
-        checkSelected(e);
-    } else if (e.target.getAttribute("data-type") === "N05") {
-        checkSelected(e);
-    } else if (e.target.getAttribute("data-type") === "N06") {
-        checkSelected(e);
-    }
+    if (e.target.nodeName !== "BUTTON") return;
+    checkSelected(e)
 })
 
 // 一般查詢渲染
-let renderList = function (list) {
+let renderList = function (dataList) {
     str = "";
-    if (list.length === 0) {
+    if (dataList.length === 0) {
         str = `<tr>
-                    <td colspan="7" class="text-center p-3">查詢不到當日的交易資訊QQ</td>
-                </tr>`;
+        <td colspan="7" class="text-center p-3">查詢不到當日的交易資訊QQ</td>
+        </tr>`;
     } else {
-        list.forEach(function (item) {
+        dataList.forEach(function (item) {
             str += `<tr>
-                        <td width="15%" class="text-left p-3">${item["作物名稱"]}</td>
-                        <td width="15%" class="text-left p-3">${item["市場名稱"]}</td>
-                        <td width="14%" class="text-left p-3">${item["上價"]}</td>
-                        <td width="14%" class="text-left p-3">${item["中價"]}</td>
-                        <td width="14%" class="text-left p-3">${item["下價"]}</td>
-                        <td width="14%" class="text-left p-3">${item["平均價"]}</td>
-                        <td width="14%" class="text-left p-3">${item["交易量"]}</td>
-                    </tr>`
+                <td width="15%" class="text-left p-3">${item["作物名稱"]}</td>
+                <td width="15%" class="text-left p-3">${item["市場名稱"]}</td>
+                <td width="14%" class="text-left p-3">${item["上價"]}</td>
+                <td width="14%" class="text-left p-3">${item["中價"]}</td>
+                <td width="14%" class="text-left p-3">${item["下價"]}</td>
+                <td width="14%" class="text-left p-3">${item["平均價"]}</td>
+                <td width="14%" class="text-left p-3">${item["交易量"]}</td>
+            </tr>`
         })
     }
-    // 資料載入中的效果
-    showList.innerHTML = "資料載入中...";
-    setTimeout(function () {
-        showList.innerHTML = str;
-    }, 500);
+    showList.innerHTML = str;
 }
 
 // 排序後結果渲染
@@ -131,64 +112,51 @@ let renderSortList = function (list) {
 jsSelect.addEventListener("change", function (e) {
     if (resultData.length === 0) {
         return;
-    } else {
-        selectArr.forEach(function (item) {
-            if (jsSelect.value.indexOf(item) !== -1) {
-                resultData.sort(function (a, b) {
-                    return a[item] - b[item]
-                })
-            }
-        })
-        // 同步js-moblie-select的選項
-        selectArr.forEach(function(item, index){
-            if(jsSelect.value.indexOf(item) !== -1){
-                jsMoblieSelect.selectedIndex = index + 1;
-            }
-        })
-        renderSortList(resultData);
     }
+    selectArr.forEach(function (item) {
+        if (jsSelect.value.indexOf(item) !== -1) {
+            resultData.sort(function (a, b) {
+                return a[item] - b[item]
+            })
+        }
+    })
+    renderSortList(resultData);
 })
 
 // 手機頁面修改排序事件
 jsMoblieSelect.addEventListener("change", function (e) {
     if (resultData.length === 0) {
         return;
-    } else {
-        selectArr.forEach(function (item) {
-            if (jsMoblieSelect.value.indexOf(item) !== -1) {
-                resultData.sort(function (a, b) {
-                    return a[item] - b[item]
-                })
-            }
-        })
-        // 同步js-select的選項
-        selectArr.forEach(function(item, index){
-            if(jsMoblieSelect.value.indexOf(item) !== -1){
-                jsSelect.selectedIndex = index + 1;
-            }
-        })
-        renderSortList(resultData);
     }
+    selectArr.forEach(function (item) {
+        if (jsMoblieSelect.value.indexOf(item) !== -1) {
+            resultData.sort(function (a, b) {
+                return a[item] - b[item]
+            })
+        }
+    })
+    renderSortList(resultData);
 })
 
 // 調整由大到小或由小到大排序
-upAndDown.addEventListener("click", function(e){
+upDownSort.addEventListener("click", function (e) {
     let getUpDown = e.target.getAttribute("data-price");
-    if(e.target.nodeName !== "I"){
+    if (e.target.nodeName !== "I" || resultData.length === 0) {
         return;
-    }else{
-        if(e.target.getAttribute("data-sort") === "up"){
-            resultData.sort(function(a, b){
-                return a[getUpDown] - b[getUpDown]
-            })
-        }else{
-            resultData.sort(function(a, b){
-                return b[getUpDown] - a[getUpDown]
-            })
-        }
-        if(resultData.length === 0){
-            return;
-        }
-        renderSortList(resultData);
+    } 
+    if (e.target.getAttribute("data-sort") === "up") {
+        resultData.sort(function (a, b) {
+            return a[getUpDown] - b[getUpDown]
+        })
+    } else {
+        resultData.sort(function (a, b) {
+            return b[getUpDown] - a[getUpDown]
+        })
     }
+    renderSortList(resultData);
 })
+
+// 網頁載入完成時直接顯示全部內容
+window.onload = function () {
+    renderList(data);
+}
